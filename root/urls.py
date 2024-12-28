091e22238092d409f7b1a1e83b471bf00e88190f
Mirshod <python3526@gmail.com>
2024-12-28 22:42:51 +0500

    Jazzmin connected + charts added (not finished)

--- root/urls.py
+++ root/urls.py
@@ -5,11 +5,12 @@ URL configuration for root project.
 from django.contrib import admin
 from django.urls import path, include
 
+# admin.site.index_template = 'admin/index.html'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pooler.urls')),
     path('users/', include('users.urls')),
     path('api/', include('files.urls')),
     path('api/', include('telegram.urls')),
-
 ]
