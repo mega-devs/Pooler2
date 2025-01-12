efe504106d442f92d58c839b3083da02baf113e0
Mirshod <python3526@gmail.com>
2025-01-12 12:55:11 +0500

    fixing CORS

--- root/settings.py
+++ root/settings.py
@@ -36,9 +36,11 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'import_export',
     'drf_yasg',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -47,6 +49,10 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+# CORS settings
+CORS_ALLOW_ALL_ORIGINS = True  # For development
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'root.urls'
 
