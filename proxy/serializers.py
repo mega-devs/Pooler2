84467f15c7ae0a62fda54db98e1181ca9325ccd0
shoxxd221 <shoxxd@mail.ru>
2025-01-18 20:29:01 +0300

    Added TextFileUploadSerializer

--- proxy/serializers.py
+++ proxy/serializers.py
@@ -1,3 +1,4 @@
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
 from .models import Proxy
@@ -8,3 +9,7 @@ class ProxySerizalizer(ModelSerializer):
         model = Proxy
         fields = ('id', 'host', 'port', 'is_active', 'country', 'country_code', 'anonymity', 'timeout')
         read_only_fields = ('id', 'is_active', 'country', 'country_code', 'anonymity', 'timeout')
+
+
+class TextFileUploadSerializer(serializers.Serializer):
+    file = serializers.FileField()
