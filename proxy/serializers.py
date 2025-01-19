3ccd9e41125675d66eed010c526030b8a6d6aef2
shoxxd221 <shoxxd@mail.ru>
2025-01-19 15:44:44 +0300

    Edit serializer for proxies

--- proxy/serializers.py
+++ proxy/serializers.py
@@ -7,7 +7,7 @@ from .models import Proxy
 class ProxySerizalizer(ModelSerializer):
     class Meta:
         model = Proxy
-        fields = ('id', 'host', 'port', 'is_active', 'country', 'country_code', 'anonymity', 'timeout')
+        fields = '__all__'
         read_only_fields = ('id', 'is_active', 'country', 'country_code', 'anonymity', 'timeout')
 
 
