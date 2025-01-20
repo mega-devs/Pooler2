bf1460e84ba24a6432ab5de9131a0497aa418291
shoxxd221 <shoxxd@mail.ru>
2025-01-20 18:58:18 +0300

    Added url for django-health check

--- root/urls.py
+++ root/urls.py
@@ -61,6 +61,7 @@ urlpatterns = [
    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
    path('prometheus/', include('django_prometheus.urls')),
+   path(r'ht/', include('health_check.urls')),
 ] + router.urls + static(main_settings.MEDIA_URL, document_root=main_settings.MEDIA_ROOT)
 
 if settings.DEBUG:
