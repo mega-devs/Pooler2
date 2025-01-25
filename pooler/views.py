c2a1b5fca6ea5fdce783b01fd1a7980da83ca00a
zeus0219 <fullstackdeveloper0219@gmail.com>
2025-01-25 21:33:26 +0300

    change run test endpoint method to GET

--- pooler/views.py
+++ pooler/views.py
@@ -43,10 +43,9 @@ def get_test_list(self):
         test_files[app] = file
     return Response({"result": test_files})
 
-@api_view(['POST'])
-@csrf_exempt
+@api_view(['GET'])
 @permission_classes([IsAuthenticated])
-@require_http_methods(["POST"])
+@require_http_methods(["GET"])
 def run_test(self, request):
     """Trigger pytest as a background task."""
     test_list = request.data.get("tests", [])
