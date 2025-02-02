54f234ee42ce7b234defca1b9b0789e14771bf54
glmzhn <galymzhan2007@gmail.com>
2025-02-02 23:29:35 +0500

    add(proxy/filters & views): added filtering by reverse + fixed filtering by country

--- proxy/filters.py
+++ proxy/filters.py
@@ -5,8 +5,14 @@ from proxy.models import Proxy
 
 class ProxyFilter(filters.FilterSet):
     max_timeout = filters.NumberFilter(field_name='timeout', lookup_expr='lte')
-    country = filters.BaseInFilter(field_name='country', lookup_expr='iexact')
+    country = filters.BaseInFilter(field_name='country', lookup_expr='in')
+    reverse = filters.BooleanFilter(method='filter_reverse', label='Reverse order')
 
     class Meta:
         model = Proxy
-        fields = ['max_timeout', 'country']
+        fields = ['max_timeout', 'country', 'reverse']
+
+    def filter_reverse(self, queryset, name, value):
+        if value:
+            return queryset.order_by('-id')
+        return queryset
